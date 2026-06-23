df8f547a514ef80d413f3e00af4cb029b40be0b2
Júlio César <juliofranca490@gmail.com>
2026-06-23 14:59:31 -0300

    flex

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  console.log('oi');
-
   return (
     <>
-      <Heading />
-      <p>Este é um parágrafo de exemplo.</p>
+      <Heading>
+        Olá, Mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Este é um parágrafo de exemplo. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Este é um parágrafo de exemplo. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit.
+      </p>
     </>
   );
 }
