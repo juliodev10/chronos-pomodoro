1a195416a630881dcae6a7f3bba471b627b27d6c
Júlio César <juliofranca490@gmail.com>
2026-06-24 08:06:50 -0300

    LabelTxt

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -14,6 +16,31 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='Task' id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button type='submit'>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
