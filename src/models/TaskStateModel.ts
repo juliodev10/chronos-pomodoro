6cf76ba1403eb6cd7bbe61c2004802db0dae9fad
Júlio César <juliofranca490@gmail.com>
2026-06-24 20:54:15 -0300

    comentarios

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,16 @@
+import type { TaskModel } from './TaskModel';
+
+//Estado -> Componente -> Filhos
+
+export type TaskStateModel = {
+  tasks: TaskModel[]; //historico, MainForm
+  secondsRemaining: number; //Countdown, histórico, MainForm, button
+  formattedSecondsRemaining: string; //titulo, countdown
+  activeTask: TaskModel | null; //Countdown, histórico, MainForm, button
+  currentCycle: number; //1 a 8/ Home
+  config: {
+    worktime: number; //MainForm
+    shortBreakTime: number; //MainForm
+    longBreakTime: number; //MainForm
+  };
+};
